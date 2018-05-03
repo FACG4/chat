87aa4143ce06d386fc85c5c3764bf4a102e04178
Mohammed <MohammedYehia@users.noreply.github.com>
2018-05-03 16:47:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const app = require("./app");
 
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
+io.set('transports', ['xhr-polling']);
+io.set('polling duration', 10);
 
 io.on("connection", function(socket) {
   socket.on("chat message", function(msg) {});

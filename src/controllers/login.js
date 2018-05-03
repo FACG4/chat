cede96f01b7995cefb1d9bffa0e27aee595d3a6b
IsraaMoqbel <isramm94@gmail.com>
2018-05-03 09:50:48 +0300

    adding webtokenjson

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,4 +1,5 @@
 const select = require('../model/queries/select');
+const { sign, verify } = require('jsonwebtoken');
 
 exports.get = (req, res) => {
   res.render("login", {
@@ -13,12 +14,28 @@ exports.post = (req, res) => {
       if(err){
         console.log(err);
       }else{
-        if(result.rowCount>0)
-        res.redirect('/');
-        else
-        res.send('<h1>Wrong name or password</h1>')
-      }
+
+        if(result.rowCount===1){
+          var userDetails = result.rows[0];
+          const SECRET = 'abc';
+          const cookie = sign(JSON.stringify(userDetails), SECRET);
+          res.cookie('user_session',cookie, { maxAge: 900000, httpOnly: true });
+          res.redirect('/');
+          res.end();
+        }else {
+          res.send('<h1>Wrong name or password</h1>')
+          res.end();
+
+        }
+
+         }
     })
+    let data = '';
+ req.on('data', (chunk) => {
+   data +=chunk;
+   console.log(data);
+
+ });
 
   }else{
     res.send('<h1>Name and Password are required</h1>')

5df448b74d38b74fdb06558fdeed32135efbd162
Ahmad <a7m4d.m.sh@gmail.com>
2018-05-03 18:35:44 +0300

    fixing token and cookie problem

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,54 +1,55 @@
-const select = require('../model/queries/select');
-const bcrypt = require('bcryptjs');
-const { sign, verify } = require('jsonwebtoken');
+const select = require("../model/queries/select");
+const bcrypt = require("bcryptjs");
+const { sign, verify } = require("jsonwebtoken");
+const cookie = require("cookie");
 
 exports.get = (req, res) => {
-  if (req.headers.cookie) {
-    verify(req.headers.cookie.split("=").slice(1).toString(), process.env.JWT_KEY, function(err, decoded) {
-        if (err) console.log(err);
-        else {
-          res.redirect('/');
-        }
-      })
-    }
-    else{
-      res.render("login", {
-        title: "Login Page"
-      });
-    }
+  if (!req.headers.cookie || !req.headers.cookie.includes("token")) {
+    res.render("login", {
+      title: "Login"
+    });
+  } else {
+    const token = cookie.parse(req.headers.cookie).token;
+    verify(token, process.env.JWT_KEY, function(err, decoded) {
+      if (err) res.status(500);
+      else {
+        res.redirect("/");
+      }
+    });
+  }
 };
+
 exports.post = (req, res) => {
-  if(req.body){
-    const {username, password} = req.body;
-    if(username.trim() !== '' && password !== ''){
-      select.selectHashedPassword(username.trim(), (err, hashedDB)=>{
-        if(err) res.send('<h1>Wrong name or password</h1>');
-        bcrypt.compare(password, hashedDB,(err, hashedResult)=> {
-          if(err) res.status(500);
-          console.log(hashedResult);
-          if(hashedResult){
-            console.log('in');
-            select.selectUserData(username,null, hashedDB, (err,result)=>{
-              console.log('in select');
-              console.log(result.rowCount);
-              if(err) return res.status(500);//data base err
-              if(result.rowCount===1){
+  if (req.body) {
+    const { username, password } = req.body;
+    if (username.trim() !== "" && password !== "") {
+      select.selectHashedPassword(username.trim(), (err, hashedDB) => {
+        if (err) res.send("<h1>Wrong name or password</h1>");
+        bcrypt.compare(password, hashedDB, (err, hashedResult) => {
+          if (err) res.status(500);
+          if (hashedResult) {
+            select.selectUserData(username, null, hashedDB, (err, result) => {
+              if (err) return res.status(500); //data base err
+              if (result.rowCount === 1) {
                 var userDetails = result.rows[0];
                 const SECRET = process.env.JWT_KEY;
-                const cookie = sign(JSON.stringify(userDetails), SECRET);
-                res.cookie('user_session',cookie, { maxAge: 900000, httpOnly: true });
-                res.redirect('/');
+                const token = sign(JSON.stringify(userDetails), SECRET);
+                res.cookie("token", token, {
+                  maxAge: 900000,
+                  httpOnly: true
+                });
+                res.redirect("/");
               }
-            })
-          }else{
-            res.send('<h1>Wrong name or password</h1>');
+            });
+          } else {
+            res.send("<h1>Wrong name or password</h1>");
           }
         });
-      })
-    }else{
-      res.send('<h1>Name and Password are required</h1>')
+      });
+    } else {
+      res.send("<h1>Name and Password are required</h1>");
     }
-  }else{
-    res.status(400).send('<h1>Invalid Data</h1>')
+  } else {
+    res.status(400).send("<h1>Invalid Data</h1>");
   }
 };

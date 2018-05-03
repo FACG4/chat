028775c8ce9dded0b5babc7c0b5c3cdde46cb403
bnazmi <6nazmi@gmail.com>
2018-05-03 16:43:54 +0300

    conflict

--- src/index.js
+++ src/index.js
@@ -5,12 +5,6 @@ var io = require("socket.io")(http);
 
 io.on("connection", function(socket) {
   socket.on("chat message", function(msg) {
-    console.log("message: " + msg);
-  });
-});
-
-io.on("connection", function(socket) {
-  socket.on("chat message", function(msg) {
     io.emit("chat message", msg);
   });
 });
